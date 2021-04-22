be3c895e3b4238233ef9b725a77f719418b4a9d4
Tomás <tommy.m@live.com.ar>
2021-04-22 16:28:22 -0300

    Buffer decoding

--- src/index.js
+++ src/index.js
@@ -149,3 +149,33 @@ export function decodeJPEGFromStream(data) {
         }
     })
 }
+
+export function decodeJPEGFromBuffer(buffer) {
+    return new Promise((res,rej)=>{
+        try {
+            let rawImageData = null
+            try {
+                rawImageData = JPEG.decode(buffer);
+            } catch(err) {
+                rej(err);
+                return
+            }
+            const bitmap = new Bitmap(rawImageData.width, rawImageData.height,{})
+            for (let x_axis = 0; x_axis < rawImageData.width; x_axis++) {
+                for (let y_axis = 0; y_axis < rawImageData.height; y_axis++) {
+                    const n = (y_axis * rawImageData.width + x_axis) * 4
+                    bitmap.setPixelRGBA_i(x_axis, y_axis,
+                        rawImageData.data[n + 0],
+                        rawImageData.data[n + 1],
+                        rawImageData.data[n + 2],
+                        rawImageData.data[n + 3]
+                    );
+                }
+            }
+            res(bitmap);
+        } catch (e) {
+            console.log(e);
+            rej(e);
+        }
+    })
+}
